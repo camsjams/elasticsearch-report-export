4262e987007e74e80872d40d1e8d9cc4a6601c50
Cameron Manavian <cameron@studymode.com>
2015-10-09 11:38:46 -0700

    switch to cleaner RC style

--- app/config.js
+++ app/config.js
@@ -1,34 +1,17 @@
-const DOC_SPACE = '\t\t';
-var argv = require('minimist')(process.argv.slice(2)),
-    config = {
-        port: 31337,
-        elastic: {
-            host: 'localhost',
-            port: 9200
-        },
-        hbs: {
-            extname: '.hbs',
-            defaultLayout: 'main'
-        },
-        formats: [
-            'csv'
-        ]
-    };
-
-function getArguments() {
-    if (argv) {
-        if (argv.help) {
-            console.log('options:\n',
-                '-p' + DOC_SPACE + '\tport (31337)\n',
-                '[--verbose]' + DOC_SPACE + 'enable more verbose logging (false)\n'
-            );
-            process.exit(1);
-        } else {
-            // extend config
-        }
-    }
-}
-
-getArguments();
-
+const APP_NAME = 'ere';
+var config = require('rc')(APP_NAME, {
+    port: 31337,
+    elastic: {
+        host: 'localhost',
+        port: 9200
+    },
+    hbs: {
+        extname: '.hbs',
+        defaultLayout: 'main'
+    },
+    formats: [
+        'csv'
+    ]
+});
+console.log(config);
 module.exports = config;
